ad8a08ed170b246d7bdb7a8a2c1ff36da22b8479
Natalia Martineli <nataliamartineli@gmail.com>
2021-11-15 12:39:43 -0300

    Added MirageJS and Axios for API mocking

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api'; //indica que todas as chamadas feitas com /api sejam captadas pelo miragejs
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        },
+      ];
+    }); //quando houver uma requisição do tipo get apara a rota /transactions
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
