abe95686f78ef022cde1d90581f8d051526d0fca
Natalia Martineli <nataliamartineli@gmail.com>
2021-11-15 21:14:08 -0300

    Added Modal, Forms and connected a mocked API

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,10 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
 import { Container } from './styles';
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+  //precisa colocar a interface dessa maneira para indicar que o useState vai armazenar uma array de objetos.
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
   useEffect(() => {
-    api.get('transactions').then((response) => console.log(response.data));
+    api
+      .get('transactions')
+      .then((response) => setTransactions(response.data.transactions));
   }, []);
 
   return (
@@ -19,18 +32,25 @@ export function TransactionsTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$ 12.000,00</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">R$ 1.200,00</td>
-            <td>Casa</td>
-            <td>15/02/2021</td>
-          </tr>
+          {transactions.map((transaction) => {
+            return (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>
+                  {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  }).format(transaction.amount)}
+                </td>
+                <td>{transaction.category}</td>
+                <td>
+                  {new Intl.DateTimeFormat('pt-BR').format(
+                    new Date(transaction.createdAt)
+                  )}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  //Criar dados já cadastrados na API
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Salário',
+          type: 'deposit',
+          category: 'Trabalho',
+          amount: 6000,
+          createdAt: new Date('2021-10-05 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1750,
+          createdAt: new Date('2021-10-12 10:00:00'),
+        },
+        {
+          id: 3,
+          title: 'Freelance',
+          type: 'deposit',
+          category: 'Trabalho',
+          amount: 3500,
+          createdAt: new Date('2021-10-15 22:00:00'),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api'; //indica que todas as chamadas feitas com /api sejam captadas pelo miragejs
 
+    //quando houver uma requisição do tipo get apara a rota /transactions
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date(),
-        },
-      ];
-    }); //quando houver uma requisição do tipo get apara a rota /transactions
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transactions', data);
+    });
   },
 });
 
